97bd3d8615ff7ba14fe75b416424ad163ff2a461
CrashVector <38360329+CrashVector@users.noreply.github.com>
2019-03-28 18:57:15 -0500

    Update index.js
    Current (broken) script that reflects new data structure with ID column

--- index.js
+++ index.js
@@ -1,50 +1,86 @@
-//table source data
-const srcData = [{
-  id: 0,
-  item: 'apple',
-  category: 'fruit'
-}, {
-  id: 0,
-  item: 'banana',
-  category: 'fruit'
-}, {
-  id: 0,
-  item: 'carrot',
-  category: 'veggie'
-}, {
-  id: 0,
-  item: 'raspberry',
-  category: 'berry'
-}, {
-  id: 0,
-  item: 'potato',
-  category: 'veggie'
-}];
-
-//DataTable initialization
-const dataTable = $('#samples').DataTable({
-  dom: 't',
-  data: srcData,
-  select: 'multi',
-  columns: Object.keys(srcData[0]).map(key => {
-    return {
-      title: key,
-      data: key
-    };
-  })
-});
-
+$(document).ready( function () {	
+  var dataTable = $('#samples').DataTable( { 
+	"processing": true,
+	"serverSide": false,
+	"pageLength": -1,
+	"lengthMenu": [[100, 250, 500, -1], [100, 250, 500, "All"]],
+	ajax: "datatables.php",     
+	columns: [ 
+      {data: '',
+       defaultContent: '0',
+       visible: false },
+      {data: '',
+       defaultContent: '',
+       orderable: false,
+       className: 'select-checkbox',
+       targets:   1
+      },
+      {title : 'Name', 'className': 'dt-left', data: 2},
+      {title : 'Region/Program', 'className': 'dt-left', data: 3},
+      {title : 'Class', 'className': 'dt-left', data: 4},
+      {title : 'Category', 'className': 'dt-left', data: 5},
+      {title : 'QC Concerns', 'className': 'dt-left', data: 6}
+    ],
+    select: {
+		style:    'multi',
+        },
+    order: ([[ 4, 'asc' ], [ 5, 'asc' ], [ 3, 'asc' ]]),
+    orderFixed: [0, 'desc'],
+	dom: 'Bfrtip',
+	buttons: [
+	 {
+            extend: 'csv',
+			fieldBoundary: '',
+            text: '<span class="fa fa-file-excel-o"></span> Download (ALL) or (SELECTED)',
+            exportOptions: {
+                columns: [ 5, 2 ],
+                modifier: {
+                    search: 'applied',
+                    order: 'applied'
+                }
+            }
+        },
+         {
+			text: 'Use Selected Library',
+            action: function ( e, dt, node, config) {
+            alert( 'This buton needs to pass the Sample Name and Category columns to php.' );
+			}
+			
+             },
+		 {
+                text: 'Upload Predefined Library',
+                action: function ( e, dt, node, conf ) {
+                    alert( 'This button needs to allow a csv file to be uploaded and passed to php.' );
+                }
+		 },
+		 {
+                text: 'Select Default Library 1',
+                action: function ( e, dt, node, conf ) {
+                    alert( 'This button will automatically check all rows that match predefined list 1 using the hidden ID column.' );
+                }
+		 },
+		 {
+                text: 'Select Default Library 2',
+                action: function ( e, dt, node, conf ) {
+                    alert( 'This button will automatically check all rows that match predefined list 2 using the hidden ID column.' );
+                }
+		 }
+  
+		
+     ]
+  } );
+  
 //grab all the unique sorted data entries from the necessary row
-const category = dataTable.column(2).data().unique().sort();
+var category = dataTable.column(2).data().unique().sort();
 
 //Drop down menu stop event propagation
 $('#samples').on('click', 'tbody td select',
   event => event.stopPropagation());
 
 //Write dropdown value into table
-const writeCell = dropdown => {
-  const currentRow = dataTable.row(dropdown.closest('tr'));
-  const rowData = currentRow.data();
+var writeCell = dropdown => {
+  var currentRow = dataTable.row(dropdown.closest('tr'));
+  var rowData = currentRow.data();
   rowData.category = dropdown.val();
   currentRow.remove();
   dataTable.row.add(rowData).draw();
@@ -52,7 +88,7 @@ const writeCell = dropdown => {
 
 dataTable.on('select', function (e, dt, type) {
   if (type === 'row') {
-    const row = dataTable.row(dt);
+    var row = dataTable.row(dt);
     $(row.node()).find('td:eq(2)').html(
       '<select>' + category.reduce((options, item) =>
         options += `<option value="${item}" ${
@@ -65,13 +101,13 @@ dataTable.on('select', function (e, dt, type) {
 
 dataTable.on('deselect', function (e, dt, type) {
   if (type === 'row') {
-    const row = dataTable.row(dt);
+    var row = dataTable.row(dt);
     writeCell($(row.node()).find('select'));
     toggleDataAndDraw(row, type, '0');
   }
 });
 
-const toggleDataAndDraw = (row, type, dataVal) => {
+var toggleDataAndDraw = (row, type, dataVal) => {
   if (type === 'row') {
     dataTable.cell({
       row: row.index(),
@@ -80,3 +116,5 @@ const toggleDataAndDraw = (row, type, dataVal) => {
     dataTable.draw();
   }
 };
+
+});
