f75ef0efefec6d41be30ed250eb618dea6e606f3
Travis Wickham <twickham37@gmail.com>
2019-04-16 21:57:30 -0500

    Fixed selection and sorting wonkiness

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 $(document).ready( function () {
-    var Category;
-    var dataTable = $('#samples').DataTable({
-      
-      //Stops the Category variable from being filled until the table has loaded
+  var Category;
+  var dataTable = $('#samples').DataTable({
+
+    //Stops the Category variable from being filled until the table has loaded
     'initComplete': function(settings, json){
-		categoryFill(settings, json);
-		},
-      
+      categoryFill(settings, json);
+    },
     'processing': true,
     'serverSide': false,
     'pageLength': -1,
@@ -70,12 +69,12 @@ $(document).ready( function () {
         data: 'QC_comment'
       }
     ],
-      
-      //creates a unique rowId by using the ID column from the database
-      rowId: function(a) {
-    return 'sampleid_' + a.ID;
-  },
-      
+
+    //creates a unique rowId by using the ID column from the database
+    rowId: function(a) {
+      return 'sampleid_' + a.ID;
+    },
+
     select: {
       style: 'multi',
     },
@@ -88,114 +87,96 @@ $(document).ready( function () {
     dom: 'Bfrtip',
     buttons: [
       {
-      extend: 'csv',
-      fieldBoundary: '',
-      text: '<span class="fa fa-file-excel-o"></span> Download (ALL) or (SELECTED)',
-      exportOptions: {
-        columns: [6, 3],
-        modifier: {
-          search: 'applied',
-          order: 'applied'
+        extend: 'csv',
+        fieldBoundary: '',
+        text: '<span class="fa fa-file-excel-o"></span> Download (ALL) or (SELECTED)',
+        exportOptions: {
+          columns: [6, 3],
+          modifier: {
+            search: 'applied',
+            order: 'applied'
+          }
+        }
+      },
+      {
+        text: 'Select Default Library 1',
+        action: function (dt) {
+          defaultselect1 (dt);
         }
       }
-    },
-    {
-      text: 'Select Default Library 1',
-      action: function (dt) {
-        defaultselect1 (dt);
-     }
-    }
     ]
   });
 
-  
-//This function adds all the unique entries from the category 
-//column into a variabl AFTER the table load is complete
+  //This function adds all the unique entries from the category
+  //column into a variabl AFTER the table load is complete
 
   function categoryFill(settings, json){
-  Category = dataTable.column(6).data().unique().sort();
+    Category = dataTable.column(6).data().unique().sort();
   }
-  
-  
-  
-//This function selects all the rows in the rowSelector1 variable (via the unique rowId)
-//when the default library 1 button is clicked
+
+  //This function selects all the rows in the rowSelector1 variable (via the unique rowId)
+  //when the default library 1 button is clicked
   //PROBLEM: Doesn't trigger datatTable.on select routine, and errors saying
   //that Cannot read property 'Category' of undefined .....needs .dt?
   defaultselect1 = function(dt){
-        var rowSelector1 = [ '#sampleid_10', '#sampleid_2', '#sampleid_401'  ];
-        dataTable.rows(rowSelector1).select();
+    var rowSelector1 = [ '#sampleid_10', '#sampleid_2', '#sampleid_401'  ];
+    dataTable.rows(rowSelector1).select();
   };
-  
-  
-//Not sure how the following functions/routines need to be broken out to fix 
-//errant behaviors (not selecting, not populating dropdown, deselect causing
-//1 or 0 to be displayed inconsistently, not saving dropdown value when selected,
-//not redrawing table, multiple selection causing dropdown issues, second check-uncheck
-//column being created when deselecting row, etc...)
-  
+
+  //Not sure how the following functions/routines need to be broken out to fix 
+  //errant behaviors (not selecting, not populating dropdown, deselect causing
+  //1 or 0 to be displayed inconsistently, not saving dropdown value when selected,
+  //not redrawing table, multiple selection causing dropdown issues, second check-uncheck
+  //column being created when deselecting row, etc...)
 
   //Not sure why this is needed?
   //Drop down menu stop event propagation
   $('#samples').on('click', 'tbody td select',
     event => event.stopPropagation());
 
-  
-  
   //Write dropdown value into table
   var writeCell = dropdown => {
     var currentRow = dataTable.row(dropdown.closest('tr'));
     var rowData = currentRow.data();
     rowData.Category = dropdown.val();
-    currentRow.remove();
-    dataTable.row.add(rowData).draw();
+    $(currentRow.node()).find('td:eq(6)').html(
+      currentRow.data().Category
+    );
+    currentRow.draw();
   };
 
-  
-  
-  
   //triggers on select/deselect to move selected rows to top of table and
   //add dropdown menu for Category column
-    //I don't understand how the select id = 'test' and '#test' pieces work. Should they
-    //be associated with the row #id? --OOOHHH, inline HTML, i see)
-    // deselecting a row where the category column has been changed triggers errors
-  
-  dataTable
-  
-    .on('select', function (e, dt, type) {
+  //I don't understand how the select id = 'test' and '#test' pieces work. Should they
+  //be associated with the row #id? --OOOHHH, inline HTML, i see)
+  // deselecting a row where the category column has been changed triggers errors
+  dataTable.on('select', function (e, dt, type) {
     if (type === 'row') {
       var row = dataTable.row(dt);
       $(row.node()).find('td:eq(6)').html(
-        
         '<select >' + Category.reduce((options, item) =>
           options += `<option value="${item}" ${
             item == row.data().Category ? 'selected' : ''}>${
             item}</option>`, '') + '</select>'
-          ).on('change', function () {
-        var optionSelected = $("option:selected", this);
+      ).on('change', function () {
+        var optionSelected = $('option:selected', this);
         // changed this.value to optionSelected()
         var valueSelected = $(optionSelected).val();
         var row = $(this).closest('tr');
         var cell = dataTable.cell(row, 6);
-        cell.data(valueSelected)
-          });
-     
-          toggleDataAndDraw(row, type, '1');
-          }
-    
-    
-        })
-  
-    .on('deselect', function (e, dt, type) {
+        cell.data(valueSelected);
+      });
+      toggleDataAndDraw(row, type, 1);
+    }
+  }).on('deselect', function (e, dt, type) {
     if (type === 'row') {
       var row = dataTable.row(dt);
       writeCell($(row.node()).find('select'));
-      toggleDataAndDraw(row, type, '0');
+      toggleDataAndDraw(row, type, 0);
     }
   });
-  
- 
- //This function is called to write the check-uncheck value and redraw the table
+
+  //This function is called to write the check-uncheck value and redraw the table
   //What is the row.index, and should/can the row #id be used instead?
   var toggleDataAndDraw = (row, type, dataVal) => {
     if (type === 'row') {
@@ -206,8 +187,5 @@ $(document).ready( function () {
       }).data(dataVal);
       dataTable.draw();
     }
-    
-   
-    
   };
 });
